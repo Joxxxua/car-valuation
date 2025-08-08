17723162786a76edd0f7aae4afd20d195b42e4b7
Joao <joaopedrosouzarodri@gmail.com>
2025-08-08 16:17:41 -0300

    feat: add auth service and interceptor fixes

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,15 +2,24 @@ import { UseInterceptors,NestInterceptor,ExecutionContext,CallHandler } from "@n
 import { plainToClass } from "class-transformer";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { UserDto } from "src/users/dtos/user.dto";
 
+interface ClassConstructor {
+    new (...args: any[]): {}
+}
+
+
+export function Serialize(dto: ClassConstructor){
+    return UseInterceptors(new SerializeInterceptor(dto))
+}
 
 export class SerializeInterceptor implements NestInterceptor{
+    constructor(private dto: any){}
+
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     
         return handler.handle().pipe(
             map((data:any) => {
-                return plainToClass(UserDto, data, {
+                return plainToClass(this.dto, data, {
                     excludeExtraneousValues: true,
                 })
                 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,30 @@
-import { Body, UseInterceptors,ClassSerializerInterceptor,Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
+import { Body,Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
-
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
-    constructor(private usersService: UsersService){}
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ){}
 
     @Post("/signup")
     createUser(@Body() body: CreateUserDto){
-        this.usersService.create(body.email, body.password)
+        return this.authService.signup(body.email, body.password);
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto){
+        return this.authService.signin(body.email,body.password)
+    }
+
     @Get("/:id")
     async findUser(@Param('id') id: string){
         const user = await this.usersService.findOne(parseInt(id))
