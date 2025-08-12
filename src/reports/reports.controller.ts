5e068440c5bedcb1eb8d0e1fadf97170341acc02
Joao <joaopedrosouzarodri@gmail.com>
2025-08-12 16:59:54 -0300

    feat: add create report with dtos for aprove and exposes, and changeApproval function

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards, Patch, Param } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 
 @Controller('reports')
@@ -10,7 +15,13 @@ export class ReportsController {
 
     @Post()
     @UseGuards(AuthGuard)
-    createReport(@Body() body: CreateReportDto){
-        return this.reportsService.create(body);
+    @Serialize(ReportDto)
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User){
+        return this.reportsService.create(body, user);
+    }
+
+    @Patch('/:id')
+    approveReport(@Param('id') id: string, @Body() body: ApproveReportDto){
+        return this.reportsService.changeApproval(id, body.approved)
     }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class ReportsService {
@@ -10,9 +11,20 @@ export class ReportsService {
         @InjectRepository(Report) private repo: Repository<Report>
     ){}
 
-    create(reportDto: CreateReportDto){
+    create(reportDto: CreateReportDto, user: User){
         const report = this.repo.create(reportDto)
+        report.user = user;
 
         return this.repo.save(report);
+    }
+
+    async changeApproval(id: string, approved: boolean){
+        const report = await this.repo.findOne({where: {id: parseInt(id)}})
+        if(!report){
+            throw new NotFoundException('Report not found');
+        }
+
+        report.approved = approved;
+        return this.repo.save(report)
     }
 }
