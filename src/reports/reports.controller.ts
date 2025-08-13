3b66e404a42f5c677e550c0a57b585466e829a3c
Joao <joaopedrosouzarodri@gmail.com>
2025-08-13 16:39:13 -0300

    feat: add current user middleware and getEstimate for the reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body, UseGuards, Patch, Param } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards, Patch, Param, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -7,11 +7,18 @@ import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService){}
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto){
+        return this.reportsService.createEstimate(query);
+    }
 
     @Post()
     @UseGuards(AuthGuard)
@@ -21,7 +28,10 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto){
         return this.reportsService.changeApproval(id, body.approved)
     }
+
+    
 }
